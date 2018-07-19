2e362f0443415dc48c013a37c859209e8ee6fd47
german gomez <germangb42@gmail.com>
2018-07-19 23:18:43 +0200

    ci

---
+++ ci/docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cargo doc
+mv target/doc doc/
+cd doc/
+
+cat docs/index.html << EOF
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0; url=lua/index.html" />
+</head>
+<body>
+    You are being redirecting to the <a href="lua/index.html">documentation page</a>...
+</body>
+</html>
+EOF
+
+git init && \
+    git remote add origin https://github.com/germangb/lua-rs.git && \
+    git checkout -b gh-pages && \
+    git commit -am "📘 Docs!" && \
+    git push origin gh-pages --force
