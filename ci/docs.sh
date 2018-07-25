bcb2a082b9cb83b00c65233a657ee00e74f0ca0b
german gomez <germangb42@gmail.com>
2018-07-25 18:17:47 +0200

    travis deploy pages

--- ci/docs.sh
+++ ci/docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCS_DIR=.doc
+DOCS_DIR=pages_deploy/
 
 cargo doc
 rm -rf $DOCS_DIR
@@ -13,14 +13,14 @@ cat << EOF > $DOCS_DIR/index.html
 <meta http-equiv="refresh" content="0; url=lua/index.html" />
 </head>
 <body>
-    You are being redirecting to the <a href="lua/index.html">documentation page</a>...
+    You are being redirected to the <a href="lua/index.html">documentation page</a>...
 </body>
 </html>
 EOF
 
 git -C $DOCS_DIR init && \
-    git -C $DOCS_DIR remote add origin https://github.com/germangb/lua-rs.git && \
+    git -C $DOCS_DIR remote add origin https://$GITHUB@github.com/germangb/lua-rs.git && \
     git -C $DOCS_DIR checkout -b gh-pages && \
     git -C $DOCS_DIR add -A && \
     git -C $DOCS_DIR commit -m "Publish docs" && \
-    git -C $DOCS_DIR push origin gh-pages --force
+    git -C $DOCS_DIR push origin gh-pages --force --quiet
